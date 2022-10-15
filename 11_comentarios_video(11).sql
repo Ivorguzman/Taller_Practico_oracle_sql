2a5baf2c5e4a202d4bf7830b3f489b42a731c00f
Ivorguzman <66047248+Ivorguzman@users.noreply.github.com>
2022-10-15 00:13:26 -0400

    respaldo 15202022

--- 11_comentarios_video(11).sql
+++ 11_comentarios_video(11).sql
@@ -1,26 +1,52 @@
+
+/*   
+   1.ESTO ES UN COMENTARIO DE BLOQUE
+*/
+
+
+ -- 2.ESTE ES UN COMETARIO EN LINEA
+ 
+/*
+
+Para aclarar algunas instrucciones, en ocasiones, necesitamos agregar comentarios.
+
+Es posible ingresar comentarios en la línea de comandos, es decir, 
+un texto que no se ejecuta; para ello se emplean dos guiones (--):
+
+ select *from libros; --mostramos los registros de libros 
+en la línea anterior, todo lo que está luego de los guiones (hacia la derecha) no se ejecuta.
+
+Para agregar varias líneas de comentarios, se coloca una barra seguida de un asterisco (/*)
+al comienzo del bloque de comentario y al finalizarlo, un asterisco seguido de una barra 
+
+*/
+
+
+/*
+
 show tables;
 create table usuarios (
 nombre varchar(30),
 clave varchar(10)
 );
 
+*/
 
-show tables;
-
-describe usuarios;
-
-drop table usuarios;
-
-show tables;
+-- drop table usuarios;
 
 create table usuarios (
 nombre varchar(30),
 clave varchar(10)
 );
 
+show tables; -- consulta para mostrar  la tabla y su contenido
 
-drop table if exists usuarios;
+describe usuarios; -- cosulta para mostrar la estrucyura de la tabla
 
-show tables;
 
-drop table if exists usuarios;
+
+
+
+
+
+
