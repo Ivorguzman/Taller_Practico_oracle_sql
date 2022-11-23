2b8b11095914da0d49c97e9e217958ad659fb3b8
Ivorguzman <66047248+Ivorguzman@users.noreply.github.com>
2022-11-23 19:35:34 -0400

    respaldo 23112022

--- 30___group_by__en_oracle_sql_video(30).sql
+++ 30___group_by__en_oracle_sql_video(30).sql
@@ -1,43 +1,165 @@
-  
- -- drop table visitantes;
-  
-  create table visitantes(
-  nombre varchar2(30),
-  edad number(2),
-  sexo char(1) default 'f',
-  domicilio varchar2(30),
-  ciudad varchar2(20) default 'Cordoba',
-  telefono varchar2(11),
-  mail varchar2(30) default 'no tiene',
-  montocompra number(6,2)
- );
+-- drop table visitantes;
+CREATE TABLE visitantes (
+ nombre        VARCHAR2(30),
+ edad          NUMBER(2),
+ sexo          CHAR(1) DEFAULT 'f',
+ domicilio     VARCHAR2(30),
+ ciudad        VARCHAR2(20) DEFAULT 'Cordoba',
+ telefono      VARCHAR2(11),
+ mail          VARCHAR2(30) DEFAULT 'no tiene',
+ montocompra   NUMBER(6, 2)
+);
 
- insert into visitantes
-  values ('Susana Molina',35,default,'Colon 123',default,null,null,59.80);
- insert into visitantes
-  values ('Marcos Torres',29,'m',default,'Carlos Paz',default,'marcostorres@hotmail.com',150.50);
- insert into visitantes
-  values ('Mariana Juarez',45,default,default,'Carlos Paz',null,default,23.90);
- insert into visitantes (nombre, edad,sexo,telefono, mail)
-  values ('Fabian Perez',36,'m','4556677','fabianperez@xaxamail.com');
- insert into visitantes (nombre, ciudad, montocompra)
-  values ('Alejandra Gonzalez','La Falda',280.50);
- insert into visitantes (nombre, edad,sexo, ciudad, mail,montocompra)
-  values ('Gaston Perez',29,'m','Carlos Paz','gastonperez1@gmail.com',95.40);
- insert into visitantes
-  values ('Liliana Torres',40,default,'Sarmiento 876',default,default,default,85);
- insert into visitantes
-  values ('Gabriela Duarte',21,null,null,'Rio Tercero',default,'gabrielaltorres@hotmail.com',321.50);
+INSERT INTO visitantes VALUES (
+ 'Susana Molina',
+ 35, DEFAULT,
+ 'Colon 123', DEFAULT,
+ NULL,
+ NULL,
+ 59.80
+);
+
+INSERT INTO visitantes VALUES (
+ 'Marcos Torres',
+ 29,
+ 'm', DEFAULT,
+ 'Carlos Paz', DEFAULT,
+ 'marcostorres@hotmail.com',
+ 150.50
+);
+
+INSERT INTO visitantes VALUES (
+ 'Mariana Juarez',
+ 45, DEFAULT, DEFAULT,
+ 'Carlos Paz',
+ NULL, DEFAULT,
+ 23.90
+);
+
+INSERT INTO visitantes (
+ nombre,
+ edad,
+ sexo,
+ telefono,
+ mail
+) VALUES (
+ 'Fabian Perez',
+ 36,
+ 'm',
+ '4556677',
+ 'fabianperez@xaxamail.com'
+);
+
+INSERT INTO visitantes (
+ nombre,
+ ciudad,
+ montocompra
+) VALUES (
+ 'Alejandra Gonzalez',
+ 'La Falda',
+ 280.50
+);
+
+INSERT INTO visitantes (
+ nombre,
+ edad,
+ sexo,
+ ciudad,
+ mail,
+ montocompra
+) VALUES (
+ 'Gaston Perez',
+ 29,
+ 'm',
+ 'Carlos Paz',
+ 'gastonperez1@gmail.com',
+ 95.40
+);
+
+INSERT INTO visitantes VALUES (
+ 'Liliana Torres',
+ 40, DEFAULT,
+ 'Sarmiento 876', DEFAULT, DEFAULT, DEFAULT,
+ 85
+);
+
+INSERT INTO visitantes VALUES (
+ 'Gabriela Duarte',
+ 21,
+ NULL,
+ NULL,
+ 'Rio Tercero', DEFAULT,
+ 'gabrielaltorres@hotmail.com',
+ 321.50
+);
+
+SELECT
+ *
+FROM
+ visitantes;
+
+SELECT
+ ciudad
+FROM
+ visitantes;
+
+SELECT
+ COUNT(*)
+FROM
+ visitantes;
   
+  -- COMO TRABAJA LA FUNCION GROUP BY EN CONJUNTO CON LA FUNCION COUNT()
+
+SELECT
+ ciudad,
+ COUNT(*) AS "CTD. DE VISITANTES POR CIUDAD "
+FROM
+ visitantes
+GROUP BY
+ ciudad;
+
+SELECT
+ ciudad,
+ COUNT(telefono) AS "VISITANTES CON N° TLF."
+FROM
+ visitantes
+GROUP BY
+ ciudad;
   
-  SELECT * FROM VISITANTES;
+  -- COMO TRABAJA LA FUNCION GROUP BY EN CONJUNTO CON LA FUNCION SUM()
+
+SELECT
+ sexo,
+ SUM(montocompra) AS "TOTAL DE COMPRASPOR GENERO"
+FROM
+ visitantes
+GROUP BY
+ sexo;
   
-  SELECT CIUDAD FROM VISITANTES;
-  
-  SELECT  COUNT(*) FROM VISITANTES;
-  
-  SELECT CIUDAD,COUNT(*) AS "CTD. DE VISITANTES POR CIUDAD " FROM VISITANTES GROUP BY CIUDAD; 
-  
-  
-  
-  
+  -- ELIMINANDO LOS REGITGROS NULOS
+
+SELECT
+ sexo,
+ SUM(montocompra) AS "TOTAL DE COMPRASPOR GENERO"
+FROM
+ visitantes
+WHERE
+ sexo IS NOT NULL
+GROUP BY
+ sexo;
+   
+ -- COMO TRABAJA LA FUNCION GROUP BY CON MAS DE UN CAMPO
+
+SELECT
+ sexo,
+ ciudad,
+ MAX(montocompra) AS "VALOR MAYOR",
+ MIN(montocompra) AS "VALOR MENOR"
+FROM
+ visitantes
+WHERE
+ sexo IS NOT NULL
+ AND montocompra IS NOT NULL
+GROUP BY
+ sexo,
+ ciudad;
